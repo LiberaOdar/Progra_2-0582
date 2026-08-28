c49a03737aabc6cd3f2aacb218c4471a65fa90d9
Libera <a20243954@pucp.edu.pe>
2026-08-28 10:24:31 -0500

    delete

--- EX1_2024-1.2/MemClientes/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Bibllioteca/Funciones.h"
-
-int main() {
-    int *clienteDNI;
-    char **clienteNombre;
-    cargarClientes(clienteDNI, clienteNombre, "ArchivosDeDatos/Clientes.csv");
-    return 0;
-}
